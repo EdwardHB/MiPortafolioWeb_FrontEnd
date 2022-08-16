1715a9c0ceeb3469b147fbf93dded65c9990361e
EdwardHB <hedward1998hb@gmail.com>
2022-08-16 18:36:09 -0300

    footer

--- ProyectoIntegrador/src/app/app.module.ts
+++ ProyectoIntegrador/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardysoftComponent } from './components/hardysoft/hardysoft.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,9 @@ import { HardysoftComponent } from './components/hardysoft/hardysoft.component';
     MiInfoComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HardysoftComponent
+    HardysoftComponent,
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
