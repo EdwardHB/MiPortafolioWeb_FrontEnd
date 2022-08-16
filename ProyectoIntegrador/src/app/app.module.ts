207b5fd14f76ca91c21239f7bce8edc3552ff79e
EdwardHB <hedward1998hb@gmail.com>
2022-08-16 19:35:40 -0300

    correcciones

--- ProyectoIntegrador/src/app/app.module.ts
+++ ProyectoIntegrador/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgentinaProgramaComponent } from './components/logo-argentina-programa/logo-argentina-programa.component';
-import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { MiInfoComponent } from './components/mi-info/mi-info.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -20,7 +19,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoArgentinaProgramaComponent,
-    RedesSocialesComponent,
     BannerComponent,
     MiInfoComponent,
     ExperienciaComponent,
